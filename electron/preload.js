3ed5f5d9c71159158397f353614e1205087f7a8c
keyouts <admin@keyouts.com>
2026-08-11 21:55:36 -0400

    Delete electron directory

--- electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('dmDashDesktop', Object.freeze({
-  openCampaign: () => ipcRenderer.invoke('campaign:open'),
-  saveCampaign: payload => ipcRenderer.invoke('campaign:save', payload)
-}));
